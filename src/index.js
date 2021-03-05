2f0fba56a55f39e11cfac86d040caf039816015d
davidseeler <david.seeler@gmail.com>
2021-03-05 13:31:47 -0500

    Extract .xlsx data

--- src/index.js
+++ src/index.js
@@ -1,49 +1,101 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const electron = require("electron");
+const ipc = electron.ipcRenderer;
+const reader = require("read-excel-file");
+const input = document.getElementById("input");
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
+// Read file input
+input.addEventListener("change", () => {
+   const data = [];
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      devTools: false
-    }
-  });
+   // Extract .xlsx data by cell
+   reader(input.files[0]).then((rows) => {
+      for (let i = 1; i < rows.length; i++){
+         for (let j = 0; j < rows[i].length; j++){
+            let sql = insertStatement;
+            console.log(rows[i][j]);
+         }
+      }
+   });
+})
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+// First part of the SQL insert statement (static)
+const insertStatement = "INSERT into DBO.CAMPSESSIONS ([CampID],[PartnerID],[CampProgram],[SummerOrNonSummer],[SiteLocation],[CamperType],[StartDate],[EndDate],[CheckInTime],[CheckOutTime],[CamperDays],[CamperMeals],[VolunteerDays],[VolunteerMeals],[BillingContactName],[BillingContactEmail],[BillingContactPhone],[BillingContactAddress],[BillingContactCity],[BillingContactState],[BillingContactZip],[CampContactName],[CampContactEmail],[CampContactPhone],[CampContactAdress],[CampContactCity],[CampContactState],[CampContactZip],[CTLStaffContact],[CTLDirected],[PreArrivalFormsComplete],[IntacctCampSessionID],[ProgramNotes],[FoodServiceNotes],[FacilityNotes],[IncidentNotes],[PartnershipNotes],[ProgramNeeds],[MealsRequested],[CabinsProjected],[BuildingNeeds],[EquipmentNeeds],[FullProgramStaff],[DayStaff],[NeedsNotes],[CampCancelled],[DirMedicalTeam],[DirMedicalTeamNotes],[DirBillableHours],[DirBillableHoursNotes],[DirVolunteers],[DirStaffingRatioNotes],[DirAddlEveningProgram],[DirTshirts],[DirPhotography],[DirNotes]) VALUES (";
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+// Partner Organization dictionary
+const parterIDs = {
+   "American Lung Association": 1,
+   "Girl Scouts of Greater Atlanta": 2,
+   "Annandale Village": 3,
+   "Bethlehem Christian Academy": 4,
+   "BlazeSports America, Inc.": 5,
+   "Arthritis Foundation": 6,
+   "Camp Blue Skies Foundation": 7,
+   "FOCUS + Fragile Kids": 9,
+   "Brain Injury Association of Georgia": 10,
+   "Hearts Everywhere Reaching Out for Children, Inc.": 11,
+   "Camp Horizon, Inc.": 12,
+   "Abbey Hospice Foundation": 14,
+   "Camp Kudzu": 15,
+   "Sickle Cell Foundation of Georgia": 16,
+   "CHOA - Transplant": 17,
+   "Suicide Prevention Action Network, Georgia": 18,
+   "Children's Healthcare of Georgia": 19,
+   "Camp Sunshine, Inc.": 20,
+   "Foster Siblings Reunited": 21,
+   "Muscular Dystrophy Association": 22,
+   "Hemophilia of Georgia": 23,
+   "Georgia Celiac Foundation": 24,
+   "CHOA - CarpeDiem": 25,
+   "CHOA - Courage": 26,
+   "CHOA - Marcus": 27,
+   "CHOA - Strong4Life": 28,
+   "CHOA - Braveheart": 29,
+   "CHOA - KrazyLegs": 30,
+   "CHOA - ThalPals": 31,
+   "Developmental Disabilities Ministries": 32,
+   "Fugees Family": 33,
+   "Grace United Methodist Church": 34,
+   "Jewish Family and Career Services": 35,
+   "Kate's Club": 36,
+   "Painless But Hopeful": 38,
+   "Catholic Archdiocese of Atlanta": 39,
+   "Tourette Information Center and Support": 40,
+   "Trinity School, Inc.": 41,
+   "University of Akron": 42,
+   "Visiting Nurse Health System, Inc.": 43,
+   "Wounded Warrior Project": 44,
+   "Special Touch Ministry, Inc.": 45,
+   "The Kyle Pease Foundation": 46,
+   "CHOA - NoLimb": 49,
+   "University of Georgia": 50,
+   "Georgia Firefighters Burn Foundation, Inc.": 51,
+   "Kidz2leaders, Inc.": 52,
+   "Crohn's & Colitis Foundation": 54,
+   "Georgia PKU Connect": 55,
+   "The Tree House, Inc.": 56,
+   "Trach Me Away": 57,
+   "Marfan Foundation": 59,
+   "Walton Options": 60,
+   "Camp Corral": 62,
+   "CHOA - Crescent Moon": 63,
+   "Georgia Institute of Technology": 64,
+   "Northside Hospital": 66,
+   "Drew Charter School": 67,
+   "Friends of L'Arche Atlanta": 68,
+   "Hospice Atlanta": 71,
+   "Spectrum Autism Support": 72,
+   "The Galloway School": 73,
+   "Extra Special People": 74,
+   "Waldorf School": 75,
+   "Hope Haven": 76,
+   "Hope Loves Company": 77,
+   "Camp Twin Lakes Program": 81,
+   "Emory University": 82,
+   "Children's Healthcare of Atlanta - Unspecified Group": 83,
+   "Barrow County Chamber of Commerce": 84,
+   "Social Skills Today": 85,
+   "Brookwood Highschool": 86,
+   "Leadership Atlanta": 87,
+   "Destination Outreach": 91,
+   "Experience Camps": 92
 };
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
---
+++ src/main.js
@@ -0,0 +1,64 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+const ipc = electron.ipcMain;
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+let window;
+
+const createWindow = () => {
+  // Create the browser window.
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, "index.html"),
+    protocol: "file",
+    slashes: true
+  }));
+
+  window.on("closed", () => {
+    window = null
+  });
+
+  // Open the DevTools.
+  window.webContents.openDevTools();
+};
+
+ipc.on("test", (event, arg) => {
+  console.log(arg);
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
